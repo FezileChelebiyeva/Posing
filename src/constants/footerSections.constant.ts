3a1b19c6e909e1ec4746404875594378119675dc
FezileChelebiyeva <fezile.celebiyevaa@gmail.com>
2024-11-15 14:31:43 +0400

    footer-component, fix hero-mainbtn

---
+++ src/constants/footerSections.constant.ts
@@ -0,0 +1,50 @@
+const sections = [
+  {
+    category: "Available POS",
+    items: [
+      {
+        name: "Food Delivery",
+        path: "delivery",
+      },
+      {
+        name: "Furniture Store",
+        path: "furniture-store",
+      },
+      {
+        name: "Coffee Shop",
+        path: "coffee-shop",
+      },
+      {
+        name: "Clothing Store",
+        path: "clothing-store",
+      },
+      {
+        name: "eCommerce",
+        path: "ecommerce",
+      },
+    ],
+  },
+  {
+    category: "Company",
+    items: [
+      {
+        name: "Features",
+        path: "features",
+      },
+      {
+        name: "FAQ",
+        path: "faq",
+      },
+      {
+        name: "Privacy Policy",
+        path: "privacy-policy",
+      },
+      {
+        name: "Terms of Use",
+        path: "terms",
+      },
+    ],
+  },
+];
+
+export default sections;
--- src/layouts/footer/Footer.tsx
+++ src/layouts/footer/Footer.tsx
@@ -1,9 +1,95 @@
-import React from 'react'
-import './Footer.module.scss'
+import React from "react";
+import styles from "./Footer.module.scss";
+import facebook from "../../assets/images/footer/social-media-icons/facebook.svg";
+import twitter from "../../assets/images/footer/social-media-icons/twitter.svg";
+import instagram from "../../assets/images/footer/social-media-icons/instagram.svg";
+import sections from "../../constants/footerSections.constant";
+import Button from "../../components/shared/Button/MainButton";
+import logo_light from "../../assets/images/site-logo/logo_light.svg";
+import { Link } from "react-router-dom";
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer id={styles.footer}>
+      <div className="container">
+        <div className={styles.footer}>
+          <div className={styles.footer_info}>
+            <img src={logo_light} alt="footer-logo" />
+            <p className={styles.info_subtitle}>
+              On the other hand, we denounce with righteous indignation and
+              dislike men who are so
+            </p>
+            <div className={styles.footer_icons}>
+              <a className={styles.social_media_icon}>
+                <img src={facebook} alt="facebook" />
+              </a>
+              <a className={styles.social_media_icon}>
+                <img src={twitter} alt="twitter" />
+              </a>
+              <a className={styles.social_media_icon}>
+                <img src={instagram} alt="instagram" />
+              </a>
+            </div>
+          </div>
+          <div className={styles.category}>
+            <h2 className={styles.category_title}>Available POS</h2>
+            <ul className={styles.navlinks_lists}>
+              {sections
+                .find((section) => section.category === "Available POS")
+                ?.items.map((item, idx) => (
+                  <li key={idx} className={styles.nav_item}>
+                    <Link className={styles.footer_link} to={item.path}>
+                      {item.name}
+                    </Link>
+                  </li>
+                ))}
+            </ul>
+          </div>
 
-export default Footer
+          <div className={styles.category}>
+            <h2 className={styles.category_title}>Company</h2>
+            <ul className={styles.navlinks_lists}>
+              {sections
+                .find((section) => section.category === "Company")
+                ?.items.map((item, idx) => (
+                  <li key={idx} className={styles.nav_item}>
+                    <Link className={styles.footer_link} to={item.path}>
+                      {item.name}
+                    </Link>
+                  </li>
+                ))}
+            </ul>
+          </div>
+
+          <div className={styles.subscribe_us}>
+            <h2 className={styles.subscribe_title}>Subscribe us</h2>
+            <p className={styles.subcribe_subtitle}>
+              Get Business news, tip and solutions to your problems from our
+              experts.
+            </p>
+            <form className={styles.contact_form}>
+              <div className={styles.contact_email}>
+                <input
+                  type="email"
+                  name=""
+                  id=""
+                  placeholder="Enter your email
+"
+                  required
+                />
+              </div>
+              <div>
+                <Button
+                  type="submit"
+                  text="Subscribe"
+                  className={styles.contact_btn}
+                ></Button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
