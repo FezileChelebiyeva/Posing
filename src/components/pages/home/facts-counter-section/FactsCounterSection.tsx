29f703fd543e0c3b7f5832a8e9339aee742afca0
FezileChelebiyeva <fezile.celebiyevaa@gmail.com>
2024-11-20 02:55:52 +0400

    scroll counter

--- src/components/pages/home/facts-counter-section/FactsCounterSection.tsx
+++ src/components/pages/home/facts-counter-section/FactsCounterSection.tsx
@@ -1,10 +1,37 @@
 import styles from "./FactsCounterSection.module.scss";
 import facts from "../../../../fake-apis/facts";
 import SlotCounter from "react-slot-counter";
+import { useEffect, useRef, useState } from "react";
 
 const FactsCounterSection = () => {
+  const [isVisible, setIsVisible] = useState(false);
+  const sectionRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            setIsVisible(true);
+          }
+        });
+      },
+      { threshold: 0.8 }
+    );
+
+    if (sectionRef.current) {
+      observer.observe(sectionRef.current);
+    }
+
+    return () => {
+      if (sectionRef.current) {
+        observer.unobserve(sectionRef.current);
+      }
+    };
+  }, []);
+
   return (
-    <section id={styles.facts_counter_section}>
+    <section id={styles.facts_counter_section} ref={sectionRef}>
       <div className={styles.container_facts_section}>
         <div className={styles.facts_section}>
           <div className={styles.facts_container}>
@@ -16,19 +43,21 @@ const FactsCounterSection = () => {
                   </div>
                   <div className={styles.fact_counter}>
                     <h1 className={styles.counter_number}>
-                      <SlotCounter
-                        value={
-                          element.number >= 1000
-                            ? String(
-                                element.plus
-                                  ? element.number / 1000 + "k" + "+"
-                                  : element.number / 1000 + "k"
-                              )
-                            : element.plus
-                            ? String(element.number + "+")
-                            : element.number
-                        }
-                      />
+                      {isVisible && (
+                        <SlotCounter
+                          value={
+                            element.number >= 1000
+                              ? String(
+                                  element.plus
+                                    ? element.number / 1000 + "k" + "+"
+                                    : element.number / 1000 + "k"
+                                )
+                              : element.plus
+                              ? String(element.number + "+")
+                              : element.number
+                          }
+                        />
+                      )}
                     </h1>
                     <p className={styles.fact_title}> {element.title}</p>
                   </div>
