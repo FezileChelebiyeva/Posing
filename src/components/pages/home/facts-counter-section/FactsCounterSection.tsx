90acb211b1259b94ba89eeddc05b3e481ec9da8d
FezileChelebiyeva <fezile.celebiyevaa@gmail.com>
2024-11-20 14:49:07 +0400

    counters ress d

--- src/components/pages/home/facts-counter-section/FactsCounterSection.tsx
+++ src/components/pages/home/facts-counter-section/FactsCounterSection.tsx
@@ -35,35 +35,37 @@ const FactsCounterSection = () => {
       <div className={styles.container_facts_section}>
         <div className={styles.facts_section}>
           <div className={styles.facts_container}>
-            {facts.map((element: any, idx: number) => {
-              return (
-                <div key={idx} className={styles.fact_card}>
-                  <div className={styles.fact_icon}>
-                    <img src={element.icon_image} alt="icon-image" />
+            <div className={styles.facts_contents}>
+              {facts.map((element: any, idx: number) => {
+                return (
+                  <div key={idx} className={styles.fact_card}>
+                    <div className={styles.fact_icon}>
+                      <img src={element.icon_image} alt="icon-image" />
+                    </div>
+                    <div className={styles.fact_counter}>
+                      <h1 className={styles.counter_number}>
+                        {isVisible && (
+                          <SlotCounter
+                            value={
+                              element.number >= 1000
+                                ? String(
+                                    element.plus
+                                      ? element.number / 1000 + "k" + "+"
+                                      : element.number / 1000 + "k"
+                                  )
+                                : element.plus
+                                ? String(element.number + "+")
+                                : element.number
+                            }
+                          />
+                        )}
+                      </h1>
+                      <p className={styles.fact_title}> {element.title}</p>
+                    </div>
                   </div>
-                  <div className={styles.fact_counter}>
-                    <h1 className={styles.counter_number}>
-                      {isVisible && (
-                        <SlotCounter
-                          value={
-                            element.number >= 1000
-                              ? String(
-                                  element.plus
-                                    ? element.number / 1000 + "k" + "+"
-                                    : element.number / 1000 + "k"
-                                )
-                              : element.plus
-                              ? String(element.number + "+")
-                              : element.number
-                          }
-                        />
-                      )}
-                    </h1>
-                    <p className={styles.fact_title}> {element.title}</p>
-                  </div>
-                </div>
-              );
-            })}
+                );
+              })}
+            </div>
           </div>
         </div>
       </div>
