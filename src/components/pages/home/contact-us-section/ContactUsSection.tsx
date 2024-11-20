99e8ba68ae1ca607ddd0b8455d59e60be603cc22
FezileChelebiyeva <fezile.celebiyevaa@gmail.com>
2024-11-20 15:05:32 +0400

    contact ress

--- src/components/pages/home/contact-us-section/ContactUsSection.tsx
+++ src/components/pages/home/contact-us-section/ContactUsSection.tsx
@@ -3,6 +3,7 @@ import Button from "../../../shared/Button/MainButton";
 import contact_icon1 from "../../../../assets/images/home/contact-us-section/icons/contact_icon_1.svg";
 import contact_icon2 from "../../../../assets/images/home/contact-us-section/icons/contact_icon_2.svg";
 import contact_icon3 from "../../../../assets/images/home/contact-us-section/icons/contact_icon_3.svg";
+import ScrollAnimation from "../../../shared/Animations/ScrollAnimation/ScrollAnimation";
 const ContactUsSection = () => {
   return (
     <section id="contact_us" className={styles.contact_us_section_all}>
@@ -10,7 +11,9 @@ const ContactUsSection = () => {
         <div className={styles.contact_us_section}>
           <div className={styles.contact_details}>
             <div className={styles.section_head}>
-              <p className={styles.first_title}>Contract Us</p>
+              <ScrollAnimation>
+                <div>Contract Us</div>
+              </ScrollAnimation>
               <h1 className={styles.section_title}>
                 If you have any quiries, fill free to contact us
               </h1>
@@ -21,33 +24,35 @@ const ContactUsSection = () => {
               </p>
             </div>
             <div className={styles.detail_infos}>
-              <div className={styles.contact_kinds_info}>
-                <div>
-                  <img src={contact_icon1} alt="contact-icon" />
+              <div className={styles.div}>
+                <div className={styles.contact_kinds_info}>
+                  <div>
+                    <img src={contact_icon1} alt="contact-icon" />
+                  </div>
+                  <div>
+                    <h3>Address</h3>
+                    <p>4117 Leroy LaneHarold, KY 41635,</p>
+                  </div>
                 </div>
-                <div>
-                  <h3>Address</h3>
-                  <p>4117 Leroy LaneHarold, KY 41635,</p>
-                </div>
-              </div>
 
-              <div className={styles.contact_kinds_info}>
-                <div>
-                  <img src={contact_icon2} alt="contact-icon" />
+                <div className={styles.contact_kinds_info}>
+                  <div>
+                    <img src={contact_icon2} alt="contact-icon" />
+                  </div>
+                  <div>
+                    <h3>Contract Number</h3>
+                    <p>+606-243-4966</p>
+                  </div>
                 </div>
-                <div>
-                  <h3>Contract Number</h3>
-                  <p>+606-243-4966</p>
-                </div>
-              </div>
 
-              <div className={styles.contact_kinds_info}>
-                <div>
-                  <img src={contact_icon3} alt="contact-icon" />
-                </div>
-                <div>
-                  <h3> Email Address</h3>
-                  <p>demo@gmail.com</p>
+                <div className={styles.contact_kinds_info}>
+                  <div>
+                    <img src={contact_icon3} alt="contact-icon" />
+                  </div>
+                  <div>
+                    <h3> Email Address</h3>
+                    <p>demo@gmail.com</p>
+                  </div>
                 </div>
               </div>
             </div>
@@ -67,7 +72,7 @@ const ContactUsSection = () => {
               </div>
               <div className={styles.input_control}>
                 <input
-                  type="text"
+                  type="number"
                   name=""
                   id=""
                   placeholder="Phone number"
